f752cd4ca53ae0c220ced03e7a2cfe2a9af0ba21
Li2nga <irina.zaitceva.iz@gmail.com>
2023-07-06 13:51:31 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int a = 1000;
+        System.out.println("Значение переменной a с типом int равно 1000");
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+}
